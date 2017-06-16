d5fae9cc39eb8e3863f5b55cbb3660564a1c4329
Will Beasley <wibeasley@hotmail.com>
2017-06-16 17:04:17 -0500

    file of connectivity functions
    See #10

---
+++ utility/connectivity.R
@@ -0,0 +1,14 @@
+open_dsn_channel <- function( ) {
+  requireNamespace("RODBC")
+
+  channel <- RODBC::odbcConnect(
+      dsn = "BeeNlsLinks",
+      uid = "NlsyReadWrite",
+      pwd = "nophi"
+    )
+  testit::assert("The ODBC channel should open successfully.", channel != -1L)
+
+  return( channel )
+}
+# channel <- open_dsn_channel()
+# RODBC::odbcClose(channel); rm(channel)
