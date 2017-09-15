429dc4766c4df34225abb2864143f05034e7d7a2
Will Beasley <wibeasley@hotmail.com>
2017-09-14 23:37:12 -0500

    metadata & imports

--- dal/import-raw.R
+++ dal/import-raw.R
@@ -10,6 +10,7 @@ base::source("utility/connectivity.R")
 library(magrittr            , quietly=TRUE)
 
 # Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("glue"                  )
 requireNamespace("readr"                  )
 requireNamespace("tidyr"                  )
 requireNamespace("tibble"                 )
@@ -21,91 +22,75 @@ requireNamespace("RODBC"                  ) #For communicating with SQL Server o
 # ---- declare-globals ---------------------------------------------------------
 # Constant values that won't change.
 directory_in              <- "data-unshared/raw"
-schema_name               <- "Extract"
 
-col_types_extract <- readr::cols(
-  .default            = readr::col_integer(),
-  RecommendedRelatedness = readr::col_double()
+ds_extract <- tibble::tribble(
+  ~table_name                       , ~file_name,
+  "Extract.tblGen1Explicit"         , "nlsy79-gen1/Gen1Explicit.csv",
+  "Extract.tblGen1Implicit"         , "nlsy79-gen1/Gen1Implicit.csv",
+  "Extract.tblGen1Links"            , "nlsy79-gen1/Gen1Links.csv",
+  "Extract.tblGen1Outcomes"         , "nlsy79-gen1/Gen1Outcomes.csv",
+  "Extract.tblGen1GeocodeSanitized" , "nlsy79-gen1/Gen1GeocodeSanitized.csv",
+  # # "Process.tblLURosterGen1"         , "nlsy79-gen1/RosterGen1.csv",
+  # tblGen1MzDzDistinction2010
+  #
+  "Extract.tblGen2FatherFromGen1"   , "nlsy79-gen2/Gen2FatherFromGen1.csv",
+  "Extract.tblGen2ImplicitFather"   , "nlsy79-gen2/Gen2ImplicitFather.csv",
+  "Extract.tblGen2Links"            , "nlsy79-gen2/Gen2Links.csv",
+  "Extract.tblGen2LinksFromGen1"    , "nlsy79-gen2/Gen2LinksFromGen1.csv",
+  "Extract.tblGen2OutcomesHeight"   , "nlsy79-gen2/Gen2OutcomesHeight.csv",
+  "Extract.tblGen2OutcomesMath"     , "nlsy79-gen2/Gen2OutcomesMath.csv",
+  "Extract.tblGen2OutcomesWeight"   , "nlsy79-gen2/Gen2OutcomesWeight.csv"
 )
-col_types_mapping <- readr::cols_only(
-  name                = readr::col_character(),
-  subdirectory        = readr::col_character(),
-  upload              = readr::col_logical()
+
+col_types_default <- readr::cols(
+  .default    = readr::col_integer()
 )
+
+checkmate::assert_character(ds_extract$table_name       , min.chars=10, any.missing=F, unique=T)
+checkmate::assert_character(ds_extract$file_name        , min.chars=10, any.missing=F, unique=T)
 
 # ---- load-data ---------------------------------------------------------------
-ds_mapping <- readr::read_csv("data-public/metadata/tables/_mapping-unshared.csv", col_types=col_types_mapping)
-ds_mapping
 
-
-ds_file <- ds_mapping %>%
+ds_extract <- ds_extract %>%
   dplyr::mutate(
-    table_name    = paste0(schema_name, ".tbl", name),
-    path          = file.path(directory_in, subdirectory, paste0(name, ".csv")),
-    exists        = purrr::map_lgl(path, file.exists)
-  ) %>%
-  dplyr::filter(upload) %>%
-  dplyr::select(name, exists, dplyr::everything())
-ds_file
-
-testit::assert("All metadata files must exist.", all(ds_file$exists))
-rm(ds_mapping)
-
-# ds_entries <- ds_file %>%
-#   dplyr::select(name, path) %>%
-#   dplyr::mutate(
-#     entries = purrr::map(.$path, readr::read_csv, col_types = col_types_extract)
-#   )
-# ds_entries
-# print(object.size(ds_entries), units="MB")
-
-
-rm(directory_in) # rm(col_types_tulsa)
+    path            = file.path(directory_in, file_name),
+    extract_exist   = file.exists(path),
+    sql_truncate    = glue::glue("TRUNCATE TABLE {table_name}")
+  )
+testit::assert("All files should be found.", all(ds_extract$extract_exist))
 
 # ---- tweak-data --------------------------------------------------------------
-ds_file$table_name
-ds_file
-
 
 # ---- verify-values -----------------------------------------------------------
 # Sniff out problems
+
 
 # ---- specify-columns-to-upload -----------------------------------------------
 
 
 # ---- upload-to-db ----------------------------------------------------------
-upload_start_time <- Sys.time()
 
 channel <- open_dsn_channel()
 RODBC::odbcGetInfo(channel)
 
-for( i in seq_len(nrow(ds_file)) ) {
+for( i in seq_len(nrow(ds_extract)) ) {
+  message(glue::glue("Uploading from `{ds_extract$file_name[i]}` to `{ds_extract$table_name[i]}`."))
 
-  cat(ds_file$table_name[[i]], ":", ds_file$path[[i]], "\n")
+  d <- readr::read_csv(ds_extract$path[i], col_types=col_types_default)
 
-  d <- readr::read_csv(ds_file$path[[i]], col_types = col_types_extract)
-  print(object.size(d), units="MB")
-  print(d)
-  cat("\n")
+  RODBC::sqlQuery(channel, ds_extract$sql_truncate[i], errors=FALSE)
 
-  # summary(d)
-  # d2 <- d[1:3, ]
-
-  # RODBC::sqlSave(channel, dat=d, tablename="Extract.tblLinks2004Gen2", safer=TRUE, rownames=FALSE, append=TRUE)
-
-  result_save <- RODBC::sqlSave(
+  RODBC::sqlSave(
     channel     = channel,
     dat         = d,
-    tablename   = ds_file$table_name[[i]],
-    safer       = FALSE,       # Don't keep the existing table.
+    tablename   = ds_extract$table_name[i],
+    safer       = TRUE,       # Don't keep the existing table.
     rownames    = FALSE,
-    append      = FALSE        # Toggle this to 'TRUE' the first time a table is uploaded.
-  )
+    append      = TRUE
+  ) %>%
+  print()
 
-  cat("Save result:", result_save)
-  cat("\n---------------------------------------------------------------\n")
+  message(glue::glue("{format(object.size(d), units='MB')}"))
 }
 
 RODBC::odbcClose(channel); rm(channel)
-cat("upload_duration_in_seconds:", round(as.numeric(difftime(Sys.time(), upload_start_time, units="secs"))), "\n")
-warnings()
