0d2c6a0d2147edc43c45bfbd47641887aa780bdc
Will Beasley <wibeasley@hotmail.com>
2018-01-06 14:05:54 -0600

    tidy spaces/tabs
    so space/tab changes in the future  won't be confused w/ real changes in  the diffs.

--- BaseAssembly/EnumResponses.cs
+++ BaseAssembly/EnumResponses.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
 namespace Nls.BaseAssembly {
-	namespace EnumResponses {
-		public enum GenderBothGenerations : int {
-			InvalidSkip = -3,
-			Male = 1,
-			Female = 2,
-		}
-	}
+    namespace EnumResponses {
+        public enum GenderBothGenerations : int {
+            InvalidSkip = -3,
+            Male = 1,
+            Female = 2,
+        }
+    }
 }
 //public enum MultipleBirthType : int {//'These values come from NLS Gen1 items such as R48260.00 and R81067.00
 //   NonInterview = -5,
