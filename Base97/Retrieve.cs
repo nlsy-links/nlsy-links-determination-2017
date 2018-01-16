1d1018544381c1ff7154acd2aaf70e977ffc6412
Will Beasley <wibeasley@hotmail.com>
2018-01-16 13:14:26 -0600

    disabling retrieve functions

--- Base97/Retrieve.cs
+++ Base97/Retrieve.cs
@@ -156,73 +156,73 @@ namespace Nls.Base97 {
 			return (LinksDataSet.tblRelatedStructureRow[])dtStructure.Select(select);
 		}
 
-		internal static EnumResponses.Gen1Roster Gen1Roster1979 ( LinksDataSet.tblRelatedStructureRow drRelated, LinksDataSet.tblResponseDataTable dt ) {//LinksDataSet.tblSubjectRow drSubject1, LinksDataSet.tblSubjectRow drSubject2 ) {
-			if ( dt == null ) throw new ArgumentNullException("dt");
-			if ( drRelated == null ) throw new ArgumentNullException("drRelated");
-			const Int32 expectedSurveyYear = 1979;
-			LinksDataSet.tblSubjectRow drSubject1 = drRelated.tblSubjectRowByFK_tblRelatedStructure_tblSubject_Subject1;
-			LinksDataSet.tblSubjectRow drSubject2 = drRelated.tblSubjectRowByFK_tblRelatedStructure_tblSubject_Subject2;
+        //internal static EnumResponses.Gen1Roster Gen1Roster1979 ( LinksDataSet.tblRelatedStructureRow drRelated, LinksDataSet.tblResponseDataTable dt ) {//LinksDataSet.tblSubjectRow drSubject1, LinksDataSet.tblSubjectRow drSubject2 ) {
+        //    if ( dt == null ) throw new ArgumentNullException("dt");
+        //    if ( drRelated == null ) throw new ArgumentNullException("drRelated");
+        //    const Int32 expectedSurveyYear = 1979;
+        //    LinksDataSet.tblSubjectRow drSubject1 = drRelated.tblSubjectRowByFK_tblRelatedStructure_tblSubject_Subject1;
+        //    LinksDataSet.tblSubjectRow drSubject2 = drRelated.tblSubjectRowByFK_tblRelatedStructure_tblSubject_Subject2;
 
-			string selectToGetLoopIndex = string.Format("{0}={1} AND {2}={3} AND {4}={5}",
-				drSubject1.SubjectTag, dt.SubjectTagColumn.ColumnName,
-				(Int16)Item.IDOfOther1979RosterGen1, dt.ItemColumn.ColumnName,
-				drSubject2.SubjectID, dt.ValueColumn.ColumnName);
-			LinksDataSet.tblResponseRow[] drsForLoopIndex = (LinksDataSet.tblResponseRow[])dt.Select(selectToGetLoopIndex);
-			Trace.Assert(drsForLoopIndex.Length == 1, "Exactly one row should be returned matching Subject2");
-			Trace.Assert(drsForLoopIndex[0].SurveyYear == expectedSurveyYear, "The SurveyYear should be " + expectedSurveyYear);
-			Int32 loopIndex = drsForLoopIndex[0].LoopIndex;
+        //    string selectToGetLoopIndex = string.Format("{0}={1} AND {2}={3} AND {4}={5}",
+        //        drSubject1.SubjectTag, dt.SubjectTagColumn.ColumnName,
+        //        (Int16)Item.IDOfOther1979RosterGen1, dt.ItemColumn.ColumnName,
+        //        drSubject2.SubjectID, dt.ValueColumn.ColumnName);
+        //    LinksDataSet.tblResponseRow[] drsForLoopIndex = (LinksDataSet.tblResponseRow[])dt.Select(selectToGetLoopIndex);
+        //    Trace.Assert(drsForLoopIndex.Length == 1, "Exactly one row should be returned matching Subject2");
+        //    Trace.Assert(drsForLoopIndex[0].SurveyYear == expectedSurveyYear, "The SurveyYear should be " + expectedSurveyYear);
+        //    Int32 loopIndex = drsForLoopIndex[0].LoopIndex;
 
-			string selectToRoster = string.Format("{0}={1} AND {2}={3} AND {4}={5}",
-				drSubject1.SubjectTag, dt.SubjectTagColumn.ColumnName,
-				(Int16)Item.RosterGen1979, dt.ItemColumn.ColumnName,
-				loopIndex, dt.LoopIndexColumn.ColumnName);
-			LinksDataSet.tblResponseRow[] drsForRoster = (LinksDataSet.tblResponseRow[])dt.Select(selectToRoster);
-			Trace.Assert(drsForRoster.Length == 1, "Exactly one row should be returned for the Roster relationship to Subject2");
-			Trace.Assert(drsForRoster[0].SurveyYear == expectedSurveyYear, "The SurveyYear should be " + expectedSurveyYear);
-			EnumResponsesGen1.Gen1Roster relationship = (EnumResponsesGen1.Gen1Roster)drsForRoster[0].Value;
-			return relationship;
-		}
-		internal static Int32 SubjectTagFromSubjectID( Int32 subjectID, LinksDataSet dsLinks ) {
-			if ( dsLinks == null ) throw new ArgumentNullException("dsLinks");
-			if ( dsLinks.tblSubject.Count <= 0 ) throw new InvalidOperationException("There should be more than one row in tblSubject.");
+        //    string selectToRoster = string.Format("{0}={1} AND {2}={3} AND {4}={5}",
+        //        drSubject1.SubjectTag, dt.SubjectTagColumn.ColumnName,
+        //        (Int16)Item.Roster, dt.ItemColumn.ColumnName,
+        //        loopIndex, dt.LoopIndexColumn.ColumnName);
+        //    LinksDataSet.tblResponseRow[] drsForRoster = (LinksDataSet.tblResponseRow[])dt.Select(selectToRoster);
+        //    Trace.Assert(drsForRoster.Length == 1, "Exactly one row should be returned for the Roster relationship to Subject2");
+        //    Trace.Assert(drsForRoster[0].SurveyYear == expectedSurveyYear, "The SurveyYear should be " + expectedSurveyYear);
+        //    EnumResponsesGen1.Gen1Roster relationship = (EnumResponsesGen1.Gen1Roster)drsForRoster[0].Value;
+        //    return relationship;
+        //}
+        //internal static Int32 SubjectTagFromSubjectID( Int32 subjectID, LinksDataSet dsLinks ) {
+        //    if ( dsLinks == null ) throw new ArgumentNullException("dsLinks");
+        //    if ( dsLinks.tblSubject.Count <= 0 ) throw new InvalidOperationException("There should be more than one row in tblSubject.");
 
-			string select = string.Format("{0} = {1}",
-				 dsLinks.tblSubject.SubjectIDColumn.ColumnName, subjectID);
-			LinksDataSet.tblSubjectRow[] drs = (LinksDataSet.tblSubjectRow[])dsLinks.tblSubject.Select(select);
-			Trace.Assert(drs.Length == 1, "The should be exactly one SubjectRow retreived.");
-			return drs[0].SubjectTag;
-		}
-		internal static LinksDataSet.tblMzManualRow MzManualRecord ( Int32 subject1Tag, Int32 subject2Tag, LinksDataSet dsLinks ) {
-			LinksDataSet.tblSubjectRow dr1 = dsLinks.tblSubject.FindBySubjectTag(subject1Tag);
-			LinksDataSet.tblSubjectRow dr2 = dsLinks.tblSubject.FindBySubjectTag(subject2Tag);
-			return MzManualRecord(dr1, dr2, dsLinks);
-		}
-		internal static LinksDataSet.tblMzManualRow MzManualRecord ( LinksDataSet.tblSubjectRow dr1, LinksDataSet.tblSubjectRow dr2, LinksDataSet dsLinks ) {
-			string select = string.Format("{0}={1} AND {2}={3}",
-				dr1.SubjectTag, dsLinks.tblMzManual.SubjectTag_S1Column.ColumnName,
-				dr2.SubjectTag, dsLinks.tblMzManual.SubjectTag_S2Column.ColumnName);
-			LinksDataSet.tblMzManualRow[] drs = (LinksDataSet.tblMzManualRow[])dsLinks.tblMzManual.Select(select);
-			switch ( drs.Length ) {
-				case 0:
-					DateTime? mob1 = Mob.Retrieve(dr1.SubjectTag, dsLinks.tblSubjectDetails);
-					DateTime? mob2 = Mob.Retrieve(dr2.SubjectTag, dsLinks.tblSubjectDetails);
-					if ( mob1.HasValue && mob2.HasValue ) {
-						if ( (dr1.Generation == (byte)Sample.Nlsy79Gen2) || (dr1.Gender == dr2.Gender) ) {
-							Int32 daysDifferenceAbsolute = (Int32)Math.Abs(mob2.Value.Subtract(mob1.Value).TotalDays);
-							Trace.Assert(daysDifferenceAbsolute > Constants.MaxDaysBetweenTwinBirths, "If siblings have close birthdays, there should be a record in tblMzManual.");
-						}
-					}
-					else {
-						Trace.Assert(OverridesGen2.MissingMobInvalidSkip.Contains(dr1.SubjectID) || OverridesGen2.MissingMobInvalidSkip.Contains(dr2.SubjectID), "Subjects with missing MOBs should be recognized.");
-					}
-					return null;
-				case 1:
-					return drs[0];
-				default:
-					Trace.Fail("At most, one record should be retrieved.");
-					throw new InvalidOperationException();
-			}
-		}
+        //    string select = string.Format("{0} = {1}",
+        //         dsLinks.tblSubject.SubjectIDColumn.ColumnName, subjectID);
+        //    LinksDataSet.tblSubjectRow[] drs = (LinksDataSet.tblSubjectRow[])dsLinks.tblSubject.Select(select);
+        //    Trace.Assert(drs.Length == 1, "The should be exactly one SubjectRow retreived.");
+        //    return drs[0].SubjectTag;
+        //}
+        //internal static LinksDataSet.tblMzManualRow MzManualRecord ( Int32 subject1Tag, Int32 subject2Tag, LinksDataSet dsLinks ) {
+        //    LinksDataSet.tblSubjectRow dr1 = dsLinks.tblSubject.FindBySubjectTag(subject1Tag);
+        //    LinksDataSet.tblSubjectRow dr2 = dsLinks.tblSubject.FindBySubjectTag(subject2Tag);
+        //    return MzManualRecord(dr1, dr2, dsLinks);
+        //}
+        //internal static LinksDataSet.tblMzManualRow MzManualRecord ( LinksDataSet.tblSubjectRow dr1, LinksDataSet.tblSubjectRow dr2, LinksDataSet dsLinks ) {
+        //    string select = string.Format("{0}={1} AND {2}={3}",
+        //        dr1.SubjectTag, dsLinks.tblMzManual.SubjectTag_S1Column.ColumnName,
+        //        dr2.SubjectTag, dsLinks.tblMzManual.SubjectTag_S2Column.ColumnName);
+        //    LinksDataSet.tblMzManualRow[] drs = (LinksDataSet.tblMzManualRow[])dsLinks.tblMzManual.Select(select);
+        //    switch ( drs.Length ) {
+        //        case 0:
+        //            DateTime? mob1 = Mob.Retrieve(dr1.SubjectTag, dsLinks.tblSubjectDetails);
+        //            DateTime? mob2 = Mob.Retrieve(dr2.SubjectTag, dsLinks.tblSubjectDetails);
+        //            if ( mob1.HasValue && mob2.HasValue ) {
+        //                if ( (dr1.Generation == (byte)Sample.Nlsy79Gen2) || (dr1.Gender == dr2.Gender) ) {
+        //                    Int32 daysDifferenceAbsolute = (Int32)Math.Abs(mob2.Value.Subtract(mob1.Value).TotalDays);
+        //                    Trace.Assert(daysDifferenceAbsolute > Constants.MaxDaysBetweenTwinBirths, "If siblings have close birthdays, there should be a record in tblMzManual.");
+        //                }
+        //            }
+        //            else {
+        //                Trace.Assert(OverridesGen2.MissingMobInvalidSkip.Contains(dr1.SubjectID) || OverridesGen2.MissingMobInvalidSkip.Contains(dr2.SubjectID), "Subjects with missing MOBs should be recognized.");
+        //            }
+        //            return null;
+        //        case 1:
+        //            return drs[0];
+        //        default:
+        //            Trace.Fail("At most, one record should be retrieved.");
+        //            throw new InvalidOperationException();
+        //    }
+        //}
 		#endregion
 	}
 }
